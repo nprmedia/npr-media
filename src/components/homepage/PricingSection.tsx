5fe1329d75a552c84fc9fb992d7f7938bf359003
nprmedia <np.media.market@gmail.com>
2025-06-12 17:36:21 -0500

    style: balance pricing grid

--- src/components/homepage/PricingSection.tsx
+++ src/components/homepage/PricingSection.tsx
@@ -20,7 +20,7 @@ export default function PricingSection() {
           </p>
         </div>
 
-        <div className="mx-auto mt-12 grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+        <div className="mx-auto mt-12 grid gap-6 md:grid-cols-2 lg:grid-cols-4">
           {pricing.tiers.map((tier, index) => (
             <motion.div
               key={tier.title}
